11acdfbf87750cb1b5b0e1ef61cc1cffd4550e40
Misuk Kim <misuke88@naver.com>
2015-01-06 17:41:13 +0900

    Restructure the code and insert input line
    We split the code into function levels. We add functions such as
    extract_date, extract_text, extract_img, make_json_file, write_file, etc.
    User can input the page numbers in order to crawl.

--- naver_blog_crawler/blog_crawler.py
+++ naver_blog_crawler/blog_crawler.py
@@ -1,67 +1,75 @@
 #-*-coding:utf-8-*-
+
+
 from bs4 import BeautifulSoup
 import urllib2
 import json
 
-
-
-i=0
-img_url = {}
-title = {}
-date = {}
-text_sep={} 
-
-
-for j in range(1,6):
-
-	url = 'http://section.blog.naver.com/sub/PostListByDirectory.nhn?option.page.currentPage='+ str(j)+ '&option.templateKind=0&option.directorySeq=29&option.viewType=default&option.orderBy=quality&option.latestOnly=1'
+def crawl_page(url):
 	page = urllib2.urlopen(url)
 	doc = BeautifulSoup(page.read())
 	images = doc.find_all("li", {"class":"add_img"})
-	
+	return images
 
+def extract_image(image):
+	img = image.find("div", {"class": "multi_img"}).img['src'].encode('utf-8')
+	return img
 
-	## file writing 
+def extract_title(image):
+	title = image.find("a").get_text().encode('utf-8')
+	return title
 
+def extract_date(image):
+	date = image.find("span",{"class": "date"}).get_text().encode('utf-8')
+	return date
+
+def extract_text(image):
+	text = image.find("div", {"class":"list_content"}).get_text().encode('utf-8').strip()
+	return text
+
+def make_json_file(obj):
+	jsonfile = json.dumps(obj, sort_keys = True, indent = 4, encoding = 'utf-8')
+	return jsonfile
+
+def write_file(jsonfile, i, p):
+	f = open('data/FoodBlog_{0:02d}.json'.format(i+(p-1)*10), 'w')
+	f.write(jsonfile)
+	f.close()
+	return 0
+
+def make_structure(image):
+
+	udate = extract_date(image)
+	utitle = extract_title(image)
+	utext = extract_text(image)
+	uimg = extract_image(image)
+
+	obj = {u"date": udate, 
+		u"title": utitle, 
+		u"text": utext, 
+		u"img": uimg
+	}
+
+	return obj
+		
+def each_page_crawler(images, p):
+	i = 1
 	for image in images:
-		img_url[i] = image.find("div", {"class": "multi_img"}).img['src'].encode('utf-8')
-		title[i] = image.find("a").get_text().encode('utf-8')
-		# title[i] = image.contents[1].get_text().encode('utf-8').strip()	
-		date[i] = image.find("span",{"class": "date"}).get_text().encode('utf-8')
-		text_sep[i]=image.find("div", {"class":"list_content"}).get_text().encode('utf-8').strip()
-		print title[i]
-		# f = open('../data/FoodBlog_{0:02d}.txt'.format(i+(j-1)*10), 'w')
-		# f.write(date[i]+'\n')
-		# f.write(title[i]+'\n')
-		# f.write(text_sep[i]+'\n')
-		# f.write(img_url[i])
-		# f.close()
-		
-		## json tranform
-		obj={u"date": date[i], 
-		u"title": title[i], 
-		u"text": text_sep[i], 
-		u"img": img_url[i]}
-		jsonfile=json.dumps(obj, sort_keys=True, indent=4, encoding='utf-8')
-		f = open('../data/FoodBlog_{0:02d}.json'.format(i+(j-1)*10), 'w')
-		f.write(jsonfile)
-		f.close()
-		
-		i = i+1
-		
+		obj = make_structure(image)
+		jsonfile = make_json_file(obj)
+		write_file(jsonfile, i, p)
+		i = i + 1
+	return 0
 
-	# if i==5:
-	# 	break
-	
-	# print title_lists.contents[3].img['src'].encode('utf-8') # console print encode error,ex) picture name= Korean
-# print img_url
+def total_page_crawler(page_need):
+	for p in range(1, page_need+1):
+		url = 'http://section.blog.naver.com/sub/PostListByDirectory.nhn?option.page.currentPage='+ str(p)+ '&option.templateKind=0&option.directorySeq=29&option.viewType=default&option.orderBy=quality&option.latestOnly=1'
+		images = crawl_page(url)
+		each_page_crawler(images, p)
 
-	
-""" 
-	save json file   
-	obj = {u"url": title_lists.contents[3].img['src'].encode('utf-8'), u"text":title_lists.get_text().encode('utf-8').replace('\n\n','\n')}
-	print (json.dumps(obj))
-	print str(obj)
+	return 0
 
-"""
+if __name__ == '__main__':
+	page_need = input('How many blog pages you need?: ')
+	total_page_crawler(page_need)
 
