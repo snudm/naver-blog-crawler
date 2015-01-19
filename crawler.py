a3df02c7ea147c667d2e9377bd439342c7ed9635
Misuk Kim <misuke88@naver.com>
2015-01-19 16:45:40 +0900

    Update Crawler Code
    We change the directory name using section numbers. And we insert the
    information about crawler time and crawler version to each object.

--- crawler.py
+++ crawler.py
@@ -7,17 +7,11 @@ import urllib2
 
 from bs4 import BeautifulSoup
 from datetime import datetime
-
-TARGETS = ['book', 'movie', 'design', 'performance', \
-           'music', 'drama', 'entertainer', 'cartoon', 'broadcasting', 'dailylife', \
-           'infantmarriage', 'pet', 'goodwriting', 'fashion', 'interior', 'cooking', \
-           'reviews', 'game', 'sport', 'picture', 'car', 'hobby', 'domestictravel', \
-           'overseastrip', 'restaurant', 'IT', 'society', 'health', 'business', \
-           'language', 'education']
+from datetime import timedelta
 
 URLBASE = 'http://section.blog.naver.com/sub/PostListByDirectory.nhn?'\
           'option.page.currentPage=%s&option.templateKind=0&option.directorySeq=%s&'\
-          'option.viewType=default&option.orderBy=date&option.latestOnly=1'
+          'option.viewType=default&option.orderBy=date&option.latestOnly=%s'
 
 def get_page(url):
     
@@ -26,7 +20,7 @@ def get_page(url):
     tmp_doc = doc.find("ul", {"class": "list_type_1"})
     return tmp_doc.find_all("li")
 
-def make_structure(div):
+def make_structure(div, crawler_version):
 
     def extract_url(div):
         return div.find("a")['href']
@@ -54,17 +48,21 @@ def make_structure(div):
 
     def extract_logNo(div):
         return div.find("input", {"class": "vLogNo"})['value']
-   
+    def extract_crawlerTime():
+        return datetime.now().strftime("%Y-%m-%d %H:%M")
+
     return {u"url": extract_url(div),
-            u"writer": extract_writer(div),
-            u"datetime": extract_date(div),
+            u"blogName": extract_writer(div),
+            u"writtenTime": extract_date(div),
             u"title": extract_title(div),
             u"content": extract_text(div),
             u"images": [{u"url":extract_image(div)}],
-            u"provider": extract_blogId(div),
-            u"aid": extract_logNo(div)}
+            u"blogId": extract_blogId(div),
+            u"logNo": extract_logNo(div),
+            u"crawledTime": extract_crawlerTime(),
+            u"crawlerVersion": crawler_version}
 
-def make_json(objs, target, basedir='./data'):
+def make_json(objs, category_id, version, basedir='./data'):
        
     year = datetime.today().year
     month = datetime.today().month
@@ -73,7 +71,7 @@ def make_json(objs, target, basedir='./data'):
     minute = datetime.today().minute
     sec = datetime.today().second
 
-    targetpath = '%s/%s/%s/%02d/%02d' % (basedir, target, year, month, day)
+    targetpath = '%s/%02d/%s/%02d/%02d' % (basedir, category_id, year, month, day)
     if not os.path.exists(targetpath):
         os.makedirs(targetpath)
     
@@ -85,13 +83,13 @@ def make_json(objs, target, basedir='./data'):
     f.write(jsonstr)
     f.close()
 
-def parse_page(divs, old_urls):
+def parse_page(divs, old_urls, version):
     
     objs = []
     flag = True
 
     for i, div in enumerate(divs):
-        obj = make_structure(div)
+        obj = make_structure(div, version)
         if obj['url'] in old_urls:
             flag = False
             break
@@ -104,8 +102,8 @@ def extract_tag(divs):
     blog_ids, log_nos = [], []
     
     for obj in divs:
-        blog_ids.append(obj['provider'])
-        log_nos.append(obj['aid'])
+        blog_ids.append(obj['blogId'])
+        log_nos.append(obj['logNo'])
    
     join_str = ','.join("{\"blogId\":\"%s\",\"logNo\":\"%s\"}" \
                     % (b, l) for b, l in zip(blog_ids, log_nos))
@@ -115,84 +113,75 @@ def extract_tag(divs):
     html = json.loads(response.read())
 
     for i, obj in enumerate(html):
-        obj["tags"] = obj['tags'] 
+        divs[i]["tags"] = obj['tags'] 
     return divs
         
-def get_old_url(target, basedir='./data'):
+def get_old_url(category_id, basedir='./data'):
 
     now_year = datetime.today().year
     now_month = datetime.today().month
     now_day = datetime.today().day
-    now_hour = datetime.today().hour
-      
-    if now_hour >23:
-        now_day = now_day - 1
-        now_hour = 24
 
-    targetpath = '%s/%s/%s/%02d/%02d' % (basedir, target, now_year, now_month, now_day)
-    if os.path.exists('./%s' % targetpath):
-        filename = max(os.listdir('./%s' % targetpath))
-        PATH = '%s/%s' % (targetpath, filename)
-        json_data = open(PATH).read()
-        data = json.loads(json_data)
-        old_urls =[]
-        for i, blog in enumerate(data):
-            old_urls.extend([data[i]['url']])
-    else:
-        old_urls = []
+    FlagDir = 1
+    while (FlagDir<10):
+        
+        targetpath = '%s/%02d/%s/%02d/%02d' % (basedir, category_id, now_year, now_month, now_day)
+        if os.path.exists('./%s' % targetpath):
+            filename = max(os.listdir('./%s' % targetpath))
+            PATH = '%s/%s' % (targetpath, filename)
+            json_data = open(PATH).read()
+            data = json.loads(json_data)
+            old_urls =[]
+            for i, blog in enumerate(data):
+                old_urls.extend([data[i]['url']])
+            break
+        else: 
+            yesterday = datetime.now().date() - timedelta(days=FlagDir)
+            FlagDir += 1
+        now_year = yesterday.year
+        now_month = yesterday.month
+        now_day = yesterday.day
+
+    if FlagDir == 10:                   
+            old_urls = []
 
     return old_urls
 
-def crawl(sectionID):
+def crawl(category_id, version, ispopular):
     # TODO: auto assign `page_need`
     new_items = []
     new_urls = []
-    target = extract_sectionID(sectionID)
-    old_urls = get_old_url(target)
+    old_urls = get_old_url(category_id)
     pagenum = 1
     flag = True
     max_page = 100
     if old_urls == [] :
-        max_page = 5
+        max_page = 100
     while(flag == True and max_page >= 1):
-        
-        divs = get_page(URLBASE % (pagenum, sectionID))
-        objs, flag = parse_page(divs, old_urls)
+        divs = get_page(URLBASE % (pagenum, category_id, ispopular))
+        objs, flag = parse_page(divs, old_urls, version)
         objs_tags = extract_tag(objs)
         new_items.extend(objs_tags)
         pagenum += 1
         max_page -= 1
-
-    make_json(new_items, target)
-
-def read_section_information():
-    f = open("section_information.txt", "r")
-    lines = f.readlines()
-    tg = []
-    secID = []
-    for line in lines:
-        tg.append(line.split(",")[0].replace("'", ""))
-        secID.append(int(line.split(",")[1]))
-    return zip(tg, secID)
-
-def extract_sectionID(sectionID):
-    secList = read_section_information()
-    for i in range(0, len(secList)):
-        if secList[i][1] == sectionID:
-            idx = i 
-    return secList[idx][0]
+    if new_items != [] :
+        make_json(new_items, category_id, version)
 
 if __name__ == '__main__':
-    import argparse
+    # import argparse
 
-    targets =  ''.join('\n- %s' % t for t in TARGETS)
+    # targets =  ''.join('\n- %s' % t for t in TARGETS)
 
-    parser = argparse.ArgumentParser(description='Get input parameters.',
-                        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-t', '--target', required=True, dest='target',
-                         help='assign target category to crawl%s' % targets)
-    args = parser.parse_args()
-    if args.target not in TARGETS:
-        raise ValueError('Target values must be in%s' % targets)
+    # parser = argparse.ArgumentParser(description='Get input parameters.',
+    #                     formatter_class=argparse.RawTextHelpFormatter)
+    # parser.add_argument('-t', '--target', required=True, dest='target',
+    #                      help='assign target category to crawl%s' % targets)
+    # args = parser.parse_args()
+    # if args.target not in TARGETS:
+    #     raise ValueError('Target values must be in%s' % targets)
    
-    crawl(arg.target)
+    # crawl(arg.target)
+    version = 0.1
+    ispopular = 1
+    for category_id in range(5, 36):
+        crawl(category_id, version, ispopular) # version number add
