cff8ceec9d388733a43b1fa1a063791ea4976f37
Misuk Kim <misuke88@naver.com>
2015-01-26 17:53:03 +0900

    Add input argument(date) and remove last url.

--- blog_page_crawler.py
+++ blog_page_crawler.py
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 URLBASE = 'http://m.blog.naver.com/%s/%s'
-REPLY_URLBASE = 'http://m.blog.naver.com/CommentList.nhn?blogId=%s&logNo=%s'
 
 get_today = lambda : datetime.now()
 
@@ -25,7 +24,8 @@ def get_reply(url):
     doc  = BeautifulSoup(page.read())
     return doc.find_all("li", {"class": "persc"})
 
-def make_structure(blog_id, log_no, raw, doc, replies, crawled_time, crawler_version, encoding='utf-8'):
+def make_structure(blog_id, log_no, raw, doc, crawled_time, crawler_version,
+                                     title, written_time, url, tags, encoding='utf-8'):
     extract_crawlerTime  = lambda: get_today().strftime("%Y-%m-%d %H:%M")
     extract_category     = lambda doc: doc.find("a", {"class": "_categoryName"}).get_text().encode(encoding)
     extract_content_html = lambda doc: doc.find("div", {"id": "viewTypeSelector"})
@@ -35,27 +35,6 @@ def make_structure(blog_id, log_no, raw, doc, replies, crawled_time, crawler_ver
             return 0
         else:
             return doc.find("em", {"id": "sympathyCount"}).get_text()
-
-    def reply_json(reply):
-        if reply.find("p") != None and reply.find("div", {"class":"dsc_id"}) != None:
-            return {u"content": reply.find("p").get_text(),
-                    u"date": reply.find("span").get_text().encode("utf"),
-                    u"blogId": reply.find("div", {"class":"dsc_id"}).find("a")["href"].rsplit("blogId=", 1)[1]}
-
-    def extract_reply(replies):
-        all_replies = []
-        for reply in range(0, len(replies)):
-            tmp = reply_json(replies[reply])
-            if tmp != None:
-                retmp = replies[reply].find_all("ul", {"class":"lst_repl_sub"})
-                re_replies = []
-                for re in range(0, len(retmp)):
-                    if reply_json(retmp[re]) != None:
-                        re_replies.append(reply_json(retmp[re]))
-                if re_replies != []:
-                    tmp["reReply"] = re_replies
-                all_replies.append(tmp)
-        return all_replies
 
     def extract_images(htmls = extract_content_html(doc)):
         image_urls = []
@@ -71,9 +50,11 @@ def make_structure(blog_id, log_no, raw, doc, replies, crawled_time, crawler_ver
             u"content": [extract_content_html(doc).get_text().encode(encoding)],
             u"crawledTime": crawled_time,
             u"crawlerVersion": crawler_version,
+            u"title": title,
+            u"writtenTime": written_time,
+            u"url": url,
+            u"tags": tags,
             u"categoryName": extract_category(doc),
-            u"comments": extract_reply(replies),
-            u"commentCrawledTime": extract_crawlerTime(),
             u"sympathyCount": extract_sympathycount(doc),
             u"images": extract_images()}
 
@@ -95,16 +76,6 @@ def count_images(blog, directory_seq, basedir, seconddir = "logs"):
     f.write(str(len(blog["images"]))+'\n')
     f.close()
 
-def last_log_url(blog_id, log_no, directory_seq, basedir, seconddir = "logs"):
-    targetpath = '%s/%s' % (basedir, seconddir)
-    filename   = '%s/last_url_%02d.txt' % (targetpath, directory_seq)
-    if not os.path.exists(targetpath):
-        os.makedirs(targetpath)
-    f = open(filename, 'w')
-    url = URLBASE % (blog_id, log_no)
-    f.write(url)
-    f.close()
-
 def error_log_url(blog_id, log_no, directory_seq, basedir, seconddir = "logs"):
     now = datetime.now()
     targetpath = '%s/%s' % (basedir, seconddir)
@@ -116,22 +87,12 @@ def error_log_url(blog_id, log_no, directory_seq, basedir, seconddir = "logs"):
     f.write(url)
     f.close()
 
-def find_url_position(directory_seq, basedir, seconddir = "logs"):
-    targetpath = '%s/%s/last_url_%02d.txt' % (basedir, seconddir, directory_seq)
-    if not os.path.isfile(targetpath):
-        url = []
-    else:
-        f   = open(targetpath, 'r')
-        url = f.readline()
-        f.close()
-    return url
-
-def web_crawl(blog_id, log_no, crawled_time, crawler_version, directory_seq, basedir):
+def web_crawl(blog_id, log_no, crawled_time, crawler_version, title, 
+                        written_time, url, tags, directory_seq, basedir):
     (raw, doc) = get_page(URLBASE % (blog_id, log_no))
     if doc != None:
-        (reply_doc) = get_reply(REPLY_URLBASE % (blog_id, log_no))
-        last_log_url(blog_id, log_no, directory_seq, basedir)
-        blog = make_structure(blog_id, log_no, raw, doc, reply_doc, crawled_time, crawler_version)
+        blog = make_structure(blog_id, log_no, raw, doc, crawled_time,
+                        crawler_version, title, written_time, url, tags)
         count_images(blog, directory_seq, basedir)
         make_json(blog, blog_id, log_no,directory_seq, basedir)
     else:
@@ -142,43 +103,31 @@ def file_read(filename):
    data = json.load(json_data)
    return data
 
-def return_information(directory_seq, basedir, seconddir ="lists"):
-    now = datetime.now()
-    day = 0
+def return_information(directory_seq, basedir, date, seconddir ="lists", thirddir="texts"):
     directory_seq = int(directory_seq)
     targetpath = '%s/%s/%02d/%s/%02d/%02d'\
-                         % (basedir, seconddir, directory_seq, now.year, now.month, now.day)
-    flag = 0
-    last_url = find_url_position(directory_seq, basedir)
-    while(os.path.exists(targetpath)):
-        filenames = glob.glob('%s/*.json' % targetpath)
-        for filename in filenames:
-            items = file_read(filename)
-            for i, blog in enumerate(items):
-                current_url = URLBASE % (items[i]['blogId'], items[i]['logNo'])
-
-                # last url text file = empty or after meeting last url change
-                if last_url == [] or flag == 1:
-                    web_crawl(items[i]['blogId'],
-                              items[i]['logNo'],
-                              items[i]['crawledTime'],
-                              items[i]['crawlerVersion'],
-                              directory_seq,
-                              basedir)
-
-                elif last_url == current_url:
-                    web_crawl(items[i]['blogId'],
-                              items[i]['logNo'],
-                              items[i]['crawledTime'],
-                              items[i]['crawlerVersion'],
-                              directory_seq,
-                              basedir)
-
-                    flag = 1
-        day += 1
-        today  = now.date() - timedelta(days=day)
-        targetpath = '%s/%s/%02d/%s/%02d/%02d'\
-                         % (basedir, seconddir, directory_seq, today.year, today.month, today.day)
+                         % (basedir, seconddir, directory_seq,\
+                            int(date[0:4]), int(date[5:7]), int(date[8:10]))
+   
+    filenames = glob.glob('%s/*.json' % targetpath)
+    for filename in reversed(filenames):
+        items = file_read(filename)
+        for i, blog in enumerate(items):
+            check_targetpath = '%s/%s/%02d/%s/%02d/%02d'\
+                            % (basedir, thirddir, directory_seq,\
+                               int(date[0:4]), int(date[5:7]), int(date[8:10]))
+            check_filename = '%s-%s.json' % (items[i]['blogId'], items[i]['logNo'])
+            if not os.path.isfile('%s/%s' % (check_targetpath, check_filename)):
+                web_crawl(items[i]['blogId'],
+                          items[i]['logNo'],
+                          items[i]['crawledTime'],
+                          items[i]['crawlerVersion'],
+                          items[i]['title'],
+                          items[i]['writtenTime'],
+                          items[i]['url'],
+                          items[i]['tags'],
+                          directory_seq,
+                          basedir)
 
 if __name__ == '__main__':
     import argparse
@@ -188,10 +137,12 @@ if __name__ == '__main__':
                          help='assign target category to crawl')
     parser.add_argument('-p', '--path', dest='basedir',
                          help='assign data path')
+    parser.add_argument('-d', '--date', dest='date',
+                         help='assign date to crawl')
     args = parser.parse_args()
 
     if not args.basedir:
         args.basedir = './data'
 
-    return_information(args.directory_seq, args.basedir)
+    return_information(args.directory_seq, args.basedir, args.date)
 
